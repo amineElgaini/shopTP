2107c1c6664b401d0732bfb2064eda378f81e1a5
amineElgaini <amineelgaini1444@gmail.com>
2024-04-03 12:46:59 +0000

    add seeders and factories for tables

--- database/migrations/2024_04_03_100635_create_products_table.php
+++ database/migrations/2024_04_03_100635_create_products_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('category');
-            $table->foreign('category')
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')
                 ->references('id')
                 ->on('categories');
 
