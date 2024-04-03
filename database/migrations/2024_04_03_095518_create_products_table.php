2d3b4551fdaf8cb3fc0da2b7d6d9f91e0db77e36
amineElgaini <amineelgaini1444@gmail.com>
2024-04-03 10:02:19 +0000

    update products migration

--- database/migrations/2024_04_03_095518_create_products_table.php
+++ database/migrations/2024_04_03_095518_create_products_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->id();
 
             $table->unsignedBigInteger('category');
             $table->foreign('category')
