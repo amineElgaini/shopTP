8e129d15c5c209ccf9b26e26bcb481b791bca7e7
abdelkabir <abdkabirneami@gmail.com>
2024-04-04 13:43:49 +0000

    add file resource

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return "home";
+});
+Route::get('/products', function () {
+    return "products";
+});
+Route::get('/orders', function () {
+    return "oders";
+});
+Route::get('/about', function () {
+    return "about";
+});
